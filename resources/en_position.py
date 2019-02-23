d2bb6a4942590a6f787542630536e7ef96faa86c
Ilyas Mohamed <ilyasmohamed97@gmail.com>
2019-02-23 10:25:16 +0000

    Add english position enum and feature to allow conversion from english position to other language

---
+++ resources/en_position.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class EnPosition(Enum):
+    GK = 1
+    LB = 2
+    LWB = 3
+    CB = 4
+    RB = 5
+    RWB = 6
+    LM = 7
+    CDM = 8
+    CM = 9
+    CAM = 10
+    RM = 11
+    LW = 12
+    RW = 13
+    LF = 14
+    CF = 15
+    RF = 16
+    ST = 17
